d1b2dc184c7ceb162090d9457ab3a01f60949a81
GoofiestRex8 <rmb210005@utdallas.edu>
2023-11-04 23:49:41 -0500

    Modified the test

--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,8 @@
 public class Main {
     public static void main(String[] args)
     {
-        Customer test = new Customer(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-        CustomerEvaluation.test(test);
+        //Test with customer with id 1
+        Customer test = new Customer(1,3103,317,374,250,268468,32216.16,236251.84,1127.9,778);
+        System.out.println(test.toString());
     }
 }
