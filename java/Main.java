ec73f3370a7bcfafdf92c0b5be3aa6d914862fe9
GoofiestRex8 <rmb210005@utdallas.edu>
2023-11-04 21:13:54 -0500

    Fixed testing code

--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        Customer test = new Customer(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Customer test = new Customer(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         CustomerEvaluation.test(test);
     }
 }
