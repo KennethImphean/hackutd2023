c9b6130c43bcb244db9c36b3f9e14a8cdac6d1ba
GoofiestRex8 <rmb210005@utdallas.edu>
2023-11-05 02:04:34 -0600

    Removed code from process classes

--- java/Main.java
+++ java/Main.java
@@ -3,30 +3,12 @@ import java.io.*;
 public class Main {
     
     // TODO delete after testing
-    public static final String exampleFileName = "hackutd2023\\data\\ExampleData.csv";
+    
     public static void main(String[] args)
     {
         //Scanner scnr = new Scanner(System.in);
-        Scanner inScan = null;
-        try {
-            inScan = new Scanner(new FileInputStream(exampleFileName));
-        } catch (Exception e) {
-            System.out.println("Block 1 - " + e.getMessage());
-        }
-
-        String csvOutputFileName = "";
-        File csvFile = null;
-
-        try {
-            csvOutputFileName = createCSVOutputFile(exampleFileName, csvFile);
-            System.out.println("Output file name: " + csvOutputFileName);
-            CSVtoJSON.convert(csvOutputFileName);
-        } catch (Exception e) {
-            System.out.println("Block 2 - " + e.getMessage());
-        }
         
-
-        inScan.close();
+        
         
         
 
@@ -46,41 +28,7 @@ public class Main {
     }
 
     
-    public static String createCSVOutputFile(String inputFileName, File csvFile) throws FileNotFoundException
-    {
-        Scanner inScan = new Scanner(new FileInputStream(inputFileName));
-        String csvFileName = inputFileName.substring(0, inputFileName.length()-4) + "Output.csv";
-        csvFile = new File(csvFileName);
-        PrintWriter csvPrintWriter = new PrintWriter(csvFile);
-        
-        String input = "";
-
-        while (inScan.hasNextLine())
-        {
-            input = inScan.nextLine();
-            String[] arr = input.split(",");;
-
-            if (!Character.isDigit(input.charAt(0)))
-            {
-                csvPrintWriter.println(input);    
-            }
-            
-            else
-            {
-                Customer customer = new Customer(arr);
-                csvPrintWriter.println(customer.toString());
-            }
-        }
-        inScan.close();
-        csvPrintWriter.close();
-        return csvFileName;
-
-        
-        
-
-        
-
-    } 
+     
 
     
 }
