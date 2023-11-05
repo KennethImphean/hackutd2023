8ce9d7577cce9e4dfe1e6d6b4ea2b474a965ce0a
GoofiestRex8 <rmb210005@utdallas.edu>
2023-11-05 02:07:09 -0600

    Added class for processing example data

---
+++ java/ExampleProcess.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+
+public abstract class ExampleProcess {
+    
+    // Example file name
+    public static final String exampleFileName = "hackutd2023\\data\\ExampleData.csv";
+
+    // Process for 
+    public static void process()
+    {
+        Scanner inScan = null;
+
+        try 
+        {
+            inScan = new Scanner(new FileInputStream(exampleFileName));
+        } 
+        
+        catch (Exception e)
+            {
+                System.out.println("Block 1 - " + e.getMessage());
+            }
+
+            String csvOutputFileName = "";
+            File csvFile = null;
+
+            try
+            {
+                csvOutputFileName = createCSVOutputFile(exampleFileName, csvFile);
+                System.out.println("Output file name: " + csvOutputFileName);
+                CSVtoJSON.convert(csvOutputFileName);
+            }
+
+            catch (Exception e)
+            {
+                System.out.println("Block 2 - " + e.getMessage());
+            }
+            
+
+            inScan.close();
+
+    }
+
+    public static String createCSVOutputFile(String inputFileName, File csvFile) throws FileNotFoundException
+    {
+        Scanner inScan = new Scanner(new FileInputStream(inputFileName));
+        String csvFileName = inputFileName.substring(0, inputFileName.length()-4) + "Output.csv";
+        csvFile = new File(csvFileName);
+        PrintWriter csvPrintWriter = new PrintWriter(csvFile);
+        
+        String input = "";
+
+        while (inScan.hasNextLine())
+        {
+            input = inScan.nextLine();
+            String[] arr = input.split(",");;
+
+            if (!Character.isDigit(input.charAt(0)))
+            {
+                csvPrintWriter.println(input);    
+            }
+            
+            else
+            {
+                Customer customer = new Customer(arr);
+                csvPrintWriter.println(customer.toString());
+            }
+        }
+        inScan.close();
+        csvPrintWriter.close();
+        return csvFileName;
+
+        
+        
+
+        
+
+    }
+}
