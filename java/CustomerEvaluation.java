03a65b122ef28eb75b28ee61deeac9b3d5743700
kylanan <kylanan@gmail.com>
2023-11-04 21:09:11 -0500

    fixed evals file

--- java/CustomerEvaluation.java
+++ java/CustomerEvaluation.java
@@ -7,15 +7,4 @@ abstract class CustomerEvaluation {
         System.out.print(x.getID());
 
     }
-
-    public static boolean CreditRatingCheck(Customer x) {
-        int creditScore = x.getCreditScore();
-        if (creditScore >= 640) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
 }   
