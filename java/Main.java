a46655041d4a353073477dc28c05aef67f972c5b
GoofiestRex8 <rmb210005@utdallas.edu>
2023-11-04 20:18:51 -0500

    Added Main.java file

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Customer test = new Customer(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        CustomerEvaluation.test(test);
+    }
+}
